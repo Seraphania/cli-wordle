b7b041e3f42696372d54e56d27a187767690f232
Seraphania <strings333@hotmail.com>
2025-03-28 13:57:21 +0800

    doc(score): added dosctrings

--- wordle.py
+++ wordle.py
@@ -1,7 +1,12 @@
+"""
+Implementation of the game "Wordle"
+Designed to run in a CLI environment
+"""
 # Amanda Guest - 20147153
 # 28-March-2025
 
 def score_guess(guess, target):
+    """Score guesses by letter, +1 to score for a letter in guess being in the target word, +1 for the letter being in the correct location."""
     score = [0,0,0,0,0]
     for i in range(len(guess)):
         letter_score = target.find(guess[i])
@@ -12,7 +17,7 @@ def score_guess(guess, target):
     return tuple(score) # Just for DevRaf!
 
 guess = "world"
-target = "world"
+target = "hello"
 
 print(score_guess(guess, target))
 # Output: (2, 2, 2, 2, 2)
