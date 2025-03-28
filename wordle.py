0d7c3f997a2afff02ffddb45a8250f5bd843d6b8
Seraphania <strings333@hotmail.com>
2025-03-28 11:45:34 +0800

    feat/psuedocode score each letter

--- wordle.py
+++ wordle.py
@@ -1,7 +1,16 @@
-"""
-Amanda Guest - 20147153
-28-March-2025
-"""
+# Amanda Guest - 20147153
+# 28-March-2025
+
+
+# Pseudocode: score each letter
+#
+# for guess_letter in guess
+#   if guess_letter in target # Use find (will return the first index)
+#       increment score by 1
+#
+#   if the index value from find matches the current guess_letter index
+#       increment score by 1
+
 
 def score_guess(guess, target):
     score = (0,0,0,0,0)
