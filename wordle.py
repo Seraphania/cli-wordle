781b1e63971dc87dd76ed3bf585a02badbc2a2db
Seraphania <strings333@hotmail.com>
2025-03-28 13:42:53 +0800

    feat(score): score by letter
    Re-created the score_guess function to score words by letter
    Still needs work, it should not score extra letters, e.g.
    guess:melee / target:elect
    should score (0,1,1,1,0), not (0,1,1,1,1)

--- wordle.py
+++ wordle.py
@@ -1,27 +1,18 @@
 # Amanda Guest - 20147153
 # 28-March-2025
 
-
-# Pseudocode: score each letter
-
-for guess_letter in guess
-   if guess_letter in target # Use find (will return the first index)
-        return target_index
-        increment score by 1
-
-    if target_index == guess_letter index
-        increment score by 1
-
 def score_guess(guess, target):
-    score = (0,0,0,0,0)
-    if guess == target:
-        score = (2,2,2,2,2)
-        return score
-    else:
-        return score
+    score = [0,0,0,0,0]
+    for i in range(len(guess)):
+        print(f"guess: {guess[i]}, target: {target[i]}")
+        letter_score = target.find(guess[i])
+        if letter_score >= 0:
+            score[i] += 1
+            if guess[i] == target[i]:
+                score[i] += 1
+    return tuple(score) # Just for DevRaf!
 
 guess = "world"
-target = "hello"
+target = "world"
 
 print(score_guess(guess, target))
-# Returns (0, 0, 0, 0, 0)
