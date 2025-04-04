cd759476b59ea82d5d389b523716cd45c4d000f0
“Seraphania” <strings333@hotmail.com>
2025-04-04 13:18:38 +0800

    fix(gameloop): player instructions
    fix for Issue #3

--- wordle.py
+++ wordle.py
@@ -7,6 +7,22 @@ Designed to run in a CLI environment
 
 # TODO #3
 # Present game instructions to player (That is, display an output to the screen).
+def instructions():
+    """Define game rules"""
+
+    rules = '''\
+    *** Welcome to CLI Wordle! ***
+    Try to guess a 5 letter word in 6 tries...\n
+    If a letter is in the correct position it will be marked with an X
+    If a letter is in the word, but not in the correct position, it will be marked with a O
+    If a letter is not in the word; it will be marked with a _
+    _________________
+    EXAMPLE:
+    Target: P R I Z E
+    Guess:  P E T A L
+    Score:  X O _ _ _
+    '''
+    print(rules)
 
 # TODO #4
 # Use a sequential algorithm to read a list of valid words and a separate list of target words.
@@ -14,15 +30,11 @@ with open("./resources/target-words.txt", "r") as target_list:
     target = []
     for line in target_list:
         target.append(line.strip())
-    print(f"First 5 words of target-words.txt:\n {target[:5]}")
-    print(f"Last 5 words of target-words.txt:\n {target[-5 : len(target)]}\n")
 
 with open("./resources/all-words.txt", "r") as all_list:
     all_words = []
     for line in all_list:
         all_words.append(line.strip())
-    print(f"First 5 words of all-words.txt:\n {all_words[:5]}")
-    print(f"Last 5 words of all-words.txt:\n {all_words[-5 : len(all_words)]}")
 
 
 # TODO #5
@@ -34,6 +46,8 @@ guess = "melee"
 
 # TODO #7
 # Check if the guess that is entered is a valid guess.
+# Case insensitivity!
+
 
 # TODO #8
 # Score the guess by providing clues on each character’s match to the target word’s characters.
@@ -50,7 +64,7 @@ def score_guess(guess, target):
                 score[i] += 1
     return tuple(score) # Just for DevRaf!
 
-print(score_guess(guess, target))
+# print(score_guess(guess, target))
 
 
 # TODO #9
@@ -59,9 +73,7 @@ print(score_guess(guess, target))
 # TODO #10
 # Present a completion message to the user.
 
-
-
-
+instructions()
 
 
 
