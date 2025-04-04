feab6f6f7ad2b246d1f296e88e7741a9187982df
“Seraphania” <strings333@hotmail.com>
2025-04-04 11:03:20 +0800

    doc(score): improve formatting of score_guess docstring

--- wordle.py
+++ wordle.py
@@ -6,7 +6,9 @@ Designed to run in a CLI environment
 # 28-March-2025
 
 def score_guess(guess, target):
-    """Score guesses by letter, +1 to score for a letter in guess being in the target word, +1 for the letter being in the correct location."""
+    """Score guesses by letter, 
+        +1 to score for a letter in GUESS being in TAGET,
+        +1 for the letter being in the correct location."""
     score = [0,0,0,0,0]
     for i in range(len(guess)):
         letter_score = target.find(guess[i])
